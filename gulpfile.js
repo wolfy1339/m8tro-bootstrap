169f93a7d326d8d3c1796880e44feeed88856247
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-05-09 19:17:59 -0400

    Rename the sourcemap to .min.css

--- gulpfile.js
+++ gulpfile.js
@@ -127,7 +127,15 @@ gulp.task('less', function() {
             title: 'cleancss:'
         }))
         .pipe(cleancss())
-        .pipe(sourcemaps.write('./'))
+        .pipe(sourcemaps.write('./', {
+            mapFile: function () {
+                var name = mapFile.split('.css');
+                return name[0] + '.min.css' + name[1];
+            }
+        }))
+        .pipe(debug({
+            title: 'copy:'
+        }))
         .pipe(gulp.dest('dist/css/'));
 
     /*gulp.src('src/m8tro-extras.less')
