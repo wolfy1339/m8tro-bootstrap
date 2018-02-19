ea90767574433f5d0f1afdc0d188ae86029e5768
wolfy1339 <webmaster@wolfy1339.com>
2018-02-19 18:58:20 -0500

    Add configuration files for JS compilation

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-  "presets": ["@babel/env"]
+    presets: [
+        [
+            '@babel/env',
+            {
+                loose: true,
+                modules: false,
+                exclude: ['transform-typeof-symbol']
+            }
+        ]
+    ],
+    plugins: [
+        process.env.PLUGINS && 'transform-es2015-modules-strip',
+        '@babel/proposal-object-rest-spread'
+    ].filter(Boolean)
 };
